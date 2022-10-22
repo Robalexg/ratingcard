b3473b66b4fd85d4353d1ee07e36edf2e43f21ef
robalexg <alexg7070@yahoo.com>
2022-10-22 12:02:42 -0500

    added functionality to buttons

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,18 @@
 import './Card.css'
+import { useState } from 'react'
 
 
 const Card = () => {
+    const [rating,setRating] = useState('')
+
+    const handleClick = (e) => {
+        const ratingNum = e.target.innerHTML
+        setRating(ratingNum)
+    }
+
+    console.log(rating)
+
+
     return(
         <section className='card--container'>  
             <div className='card--starContainer'>
@@ -14,11 +25,11 @@ const Card = () => {
                 improve our offerings!
             </p>
             <div className='card--ratingContainer' >
-                <div className='card--rating'>1</div>
-                <div className='card--rating'>2</div>
-                <div className='card--rating'>3</div>
-                <div className='card--rating'>4</div>
-                <div className='card--rating'>5</div>
+                <div onClick={handleClick} className={`card--rating ${rating === "1" ? 'card--checked': ''  }`}>1</div>
+                <div onClick={handleClick} className={`card--rating ${rating === "2" ? 'card--checked': ''  }`}>2</div>
+                <div onClick={handleClick} className={`card--rating ${rating === "3" ? 'card--checked': ''  }`}>3</div>
+                <div onClick={handleClick} className={`card--rating ${rating === "4" ? 'card--checked': ''  }`}>4</div>
+                <div onClick={handleClick} className={`card--rating ${rating === "5" ? 'card--checked': ''  }`}>5</div>
             </div>
             <button className='card--button'>SUBMIT</button>
         </section>
